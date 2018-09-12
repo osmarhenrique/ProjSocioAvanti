bc7facb19b3e200e4f69a67ccd64394773db8f47
Henrique <Henrique@192.168.0.2>
2018-09-12 11:36:54 -0300

    I001

---
+++ src/Associoados/SociosAvanti.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Associoados;
+
+/**
+ *
+ * @author Henrique
+ */
+public class SociosAvanti {
+    
+    public String NOME = " ";
+    public String CPF = " ";
+    public int IDADE = 0;
+    
+}
+
---
+++ src/socioavanti/planos/PlanoPrata.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socioavanti.planos;
+
+import Planos.Planos;
+
+/**
+ *
+ * @author a400987
+ */
+public class PlanoPrata extends Planos {
+       
+}
